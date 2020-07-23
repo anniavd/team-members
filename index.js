6006b274318448c23e5e3caa247260bba1b88743
annia valdes <millerl.mb79@gmail.com>
2020-07-22 19:59:43 -0500

    add the class address and the inquirer

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const Manager=require("./lib/Manager")
+const Employee=require("./lib/Employee")
+const Intern=require("./lib/Intern")
+const Engineer=require("./lib/Engineer")
