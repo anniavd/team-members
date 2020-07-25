a03cd1d028d654d05409afff919f5a431a36a294
annia valdes <millerl.mb79@gmail.com>
2020-07-24 23:21:13 -0500

    create the employee objects (manager,engineer and Intern).create function for write a file.create a global function for call the menu on console.

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
+
 const Manager = require("./lib/Manager")
 const Employee = require("./lib/Employee")
 const Intern = require("./lib/Intern")
 const Engineer = require("./lib/Engineer")
 
-const teamMember=[];
+const teamMember=[]; //create array member's team.
+const identifier=[];
+
+//const outPageHtml =path.join(OUTPUT_DIR,"team.html")
+
+
+function ShowMenu(){
+
 // add employee
-
-
 function promptToAddEmployee() {
     inquirer.prompt([
-       {
- 
+       { 
              type: 'list',
              name: 'addEmployee',
              message: 'Would you like to add employees to your team or no.',
@@ -20,9 +26,9 @@ function promptToAddEmployee() {
          }
     ])
     .then(({ addEmployee }) => {
-        if (addEmployee === 'Engenieer') {
-            return inquirer.prompt([
-                //manager name
+        if (addEmployee === 'Engenieer') { //add a enginner to the team
+             inquirer.prompt([
+                //engineer name
                 {
                     type: 'input',
                     name: 'nameIng',
@@ -39,7 +45,7 @@ function promptToAddEmployee() {
                 //engineer ID
                 {
                     type: 'input',
-                    name: 'id',
+                    name: 'idIng',
                     message: 'What is the employee ID?',
                     validate: idInput => {           //validation the entry
                         if (idInput) {
@@ -53,7 +59,7 @@ function promptToAddEmployee() {
                 //engineer email
                 {
                     type: 'input',
-                    name: 'email',
+                    name: 'emailIng',
                     message: 'What is the email address?',
                     validate: emailInput => {
                         if (emailInput) {
@@ -67,7 +73,7 @@ function promptToAddEmployee() {
                 // engineer github user
                 {
                     type: 'input',
-                    name: 'github',
+                    name: 'githubIng',
                     message: 'What is the github username?',
                     validate: githubInput => {
                         if (githubInput) {
@@ -79,18 +85,23 @@ function promptToAddEmployee() {
                     }
                 },
                
-            ])
-            .then(promptToAddEmployee)
+            ])//create a engineer object
+            .then(answerEngineer=>{
+                const eng= new Engineer (answerEngineer.nameIng,answerEngineer.idIng,answerEngineer.emailIng,answerEngineer.githubIng);
+                teamMember.push(eng);
+               // identifier.push(answerEngineer.idIng);
+            })
+            .then(promptToAddEmployee) //call to add employee
         }
-        else if (addEmployee === 'Intern') {
+        else if (addEmployee === 'Intern') {   //add a Intern to the team
             return inquirer.prompt([
                 //Intern name
                 {
                     type: 'input',
-                    name: 'nameIng',
+                    name: 'nameInt',
                     message: 'What is your Intern name?',
-                    validate: nameIngInput => {           //validation the entry
-                        if (nameIngInput) {
+                    validate: nameInput => {           //validation the entry
+                        if (nameInput) {
                             return true;
                         } else {
                             console.log('Please enter the name!');
@@ -101,7 +112,7 @@ function promptToAddEmployee() {
                 //Intern ID
                 {
                     type: 'input',
-                    name: 'id',
+                    name: 'idInt',
                     message: 'What is the employee ID?',
                     validate: idInput => {           //validation the entry
                         if (idInput) {
@@ -115,7 +126,7 @@ function promptToAddEmployee() {
                 //Intern email
                 {
                     type: 'input',
-                    name: 'email',
+                    name: 'emailInt',
                     message: 'What is the email address?',
                     validate: emailInput => {
                         if (emailInput) {
@@ -141,11 +152,16 @@ function promptToAddEmployee() {
                     }
                 },
                 
-            ])
+            ])// create a intern object, add a list teamMenber
+            .then(answerInt=>{
+                const int= new Intern (answerInt.nameInt,answerInt.idInt,answerInt.emailInt,answerInt.schoolName);
+                teamMember.push(int);
+               // identifier.push(answerInt.idInt);
+            })
             .then(promptToAddEmployee)
         }
-        else {
-            return "";
+        else { //call function buildingTeam.Generate the file html
+            buildingTeam();
         }
 
     })  
@@ -153,11 +169,11 @@ function promptToAddEmployee() {
 
 // manager building team
  function managerQuest() {
-    return inquirer.prompt([
+     inquirer.prompt([
         //manager name
         {
             type: 'input',
-            name: 'nameTeam',
+            name: 'nameManager',
             message: 'What is your team manager’s name?',
             validate: nameTeamInput => {           //validation the entry
                 if (nameTeamInput) {
@@ -168,7 +184,7 @@ function promptToAddEmployee() {
                 }
             }
         },
-        //employee ID
+        //manager ID
         {
             type: 'input',
             name: 'id',
@@ -182,7 +198,7 @@ function promptToAddEmployee() {
                 }
             }
         },
-        //employee email
+        //manager email
         {
             type: 'input',
             name: 'email',
@@ -210,9 +226,30 @@ function promptToAddEmployee() {
                 }
             }
         }               
-    ])//;
+    ]) //create manager object
+    .then(answers => {
+        const manager = new Manager(answers.nameManager, answers.id, answers.email, answers.officeNum);
+        teamMember.push(manager);
+        //identifier.push(answers.id);
+       
+      })
+          //call to add employee to the team
     .then(promptToAddEmployee)
  }
 
+ //create a file html
+ function buildingTeam() {
+    
+    console.log(teamMember)
+    fs.writeFileSync("./dist/team.html",teamMember,"utf-8");
+  }
 
-managerQuest();
+  //call to show the manager question
+ managerQuest();
+
+
+}
+
+
+//call to show in console the menu
+ShowMenu();
