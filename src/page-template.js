e8cf14e722b1f5bf5111841125e4f069bd0e45df
annia valdes <millerl.mb79@gmail.com>
2020-07-26 18:57:34 -0500

    update style for html

--- src/page-template.js
+++ src/page-template.js
@@ -1,83 +1,95 @@
 const Engineer = require("../lib/Engineer");
 
-
+//create manager card
 const generateManager = teamMember => {
-    if (!teamMember) {
-        return '';
-    }
+  if (!teamMember) {
+    return '';
+  }
 
-    return `
-    <div class="card" style="width: 18rem;">
-    <div class="card-header">
+  return `
+    
+    <div class="col-md-4">
+    <div class="card m-4 " style="width: 18rem;">
+    <div class="card-header" style=" background-color: rgb(247, 211, 112);">
     <h3>Manager<h3>
     <h4>${teamMember.manager.name}<h4>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">ID:${teamMember.manager.id}</li>
-      <li class="list-group-item">Email: ${teamMember.manager.email}</li>              
-      <li class="list-group-item">${teamMember.manager.officeNumber}</li>
+      <li class="list-group-item"><i class="far fa-id-badge"> ${teamMember.manager.id}</i></li>
+      <li class="list-group-item"><i class="fas fa-envelope-square"></i><a href="mailto:" + ${teamMember.manager.email}> ${teamMember.manager.email}</a></li>              
+      <li class="list-group-item"><i class="fas fa-phone-volume"></i> ${teamMember.manager.officeNumber}</li>
     </ul>
   </div>
+  </div>
+ 
     `;
 };
 
-
+//create engineer cards
 const generateEngineer = (teamMember) => {
-   
-    if (!teamMember.engineer) {
-        return '';
-    }
-    else {
-        let html = '';
-        for (let i = 0;i<teamMember.engineer.length;i++) {
-            html += `            
-    <div class="card" style="width: 18rem; ">
-    <div class="card-header">
+
+  if (!teamMember.engineer) {
+    return '';
+  }
+  else {
+    let html = '';
+    for (let i = 0; i < teamMember.engineer.length; i++) {
+      html += ` 
+    
+    <div class="col-md-4">                  
+    <div class="card m-4 " style="width: 18rem; ">
+    <div class="card-header" style=" background-color: rgb(247, 211, 112);">
     <h3>Engineer<h3>
     <h4>${teamMember.engineer[i].name}<h4>
     </div>
     <ul class="list-group list-group-flush">          
-      <li class="list-group-item">ID:${teamMember.engineer[i].id}</li>              
-      <li class="list-group-item"><a href="#">Email: ${teamMember.engineer[i].email}</a></li>
-      <li class="list-group-item"><a href="https://github.com/${teamMember.engineer[i].github}" target="_blank">Github: ${teamMember.engineer[i].github}</a></li>
+      <li class="list-group-item"><i class="far fa-id-badge"> ${teamMember.engineer[i].id}</i></li>              
+      <li class="list-group-item"><i class="fas fa-envelope-square"></i><a href="mailto:" +${teamMember.engineer[i].email}> ${teamMember.engineer[i].email}</a></li>
+      <li class="list-group-item"><i class="fab fa-github"></i><a href="https://github.com/${teamMember.engineer[i].github}" target="_blank"> ${teamMember.engineer[i].github}</a></li>
     </ul>
-  </div>   `
-        }
-        return html;
+  </div> 
+  </div> 
+  `
     }
+    return html;
+  }
 };
-
+//create intern cards
 const generateIntern = teamMember => {
-  
-    if (!teamMember.intern) {
-        return '';
-    }
-    else {
-       let html='';
-        for (let i = 0; i<teamMember.intern.length; i++) {
-         html+=`      
-<div class="card" style="width: 18rem; ">
-    <div class="card-header">
+
+  if (!teamMember.intern) {
+    return '';
+  }
+  else {
+    let html = '';
+    for (let i = 0; i < teamMember.intern.length; i++) {
+      html += ` 
+
+<div class="col-md-4">             
+<div class="card m-4 " style="width: 18rem; ">
+    <div class="card-header" style=" background-color:rgb(247, 211, 112);">
     <h3>Intern<h3>   
     <h4>${teamMember.intern[i].name}<h4>   
     </div>
     <ul class="list-group list-group-flush">          
-      <li class="list-group-item">ID: ${teamMember.intern[i].id}</li>              
-      <li class="list-group-item"><a href="#">Email: ${teamMember.intern[i].email}</a></li>
-      <li class="list-group-item">School name: ${teamMember.intern[i].nameSchool}</li>
+      <li class="list-group-item"><i class="far fa-id-badge"> ${teamMember.intern[i].id}</i> </li>              
+      <li class="list-group-item"><i class="fas fa-envelope-square"></i><a href="mailto:" +${teamMember.intern[i].email}> ${teamMember.intern[i].email}</a></li>
+      <li class="list-group-item"><i class="fas fa-graduation-cap"></i> ${teamMember.intern[i].nameSchool}</li>
     </ul>
-  </div>  `
-        }
-        return html;
+  </div> 
+  </div> 
+   `
     }
+    return html;
+  }
 };
 
 
 
 //generate the html
 module.exports = (teamMember) => {
-    
-    return `
+
+  return `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -85,24 +97,30 @@ module.exports = (teamMember) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@1&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+            integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css"/>
 
         <title>TEAM MEMBERS</title>
     </head>
     <body>
     <header>
-    <h1> Team Members</h1>
+    <nav class="navbar  justify-content-center" style="background-color:rgb(189, 157, 70);">
+        <h1 class="navbar-text text-body">Team Members</h1>
+   </nav>
    </header>
-   <main class="container ">   
-      ${generateManager(teamMember)}       
+   <main class="container ">  
+   <div class="row mb-2 justify-content-center"> 
+      ${generateManager(teamMember)}  
+   </div>     
+   <div class="row mb-2 justify-content-center">    
       ${generateEngineer(teamMember)} 
+  </div>   
+  <div class="row mb-2 justify-content-center">    
       ${generateIntern(teamMember)}   
-                          
-    </main>
-
-      <footer class="container text-center py-3">
-       
-      </footer>
+  </div>                     
+    </main>         
+     
     </body>
     </html>
     `
