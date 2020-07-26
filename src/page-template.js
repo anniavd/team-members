8e31902729b6640726195bb68e7cb76e28d8b780
annia valdes <millerl.mb79@gmail.com>
2020-07-26 12:26:16 -0500

    add the links to fonts and style.css

--- src/page-template.js
+++ src/page-template.js
@@ -83,7 +83,9 @@ module.exports = (teamMember) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@1&family=Playfair+Display:wght@500&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css"/>
 
         <title>TEAM MEMBERS</title>
     </head>
